03bcc582478a38745129d6bbb6420858d3db8f95
Yon <yaguilar213@gmail.com>
2015-11-09 12:39:36 -0800

    files set up/template in progress

---
+++ assets/JS/player.js
@@ -0,0 +1,14 @@
+var audioPlayer = {
+  audioData:{
+    currentSong: -1,
+    songs:[]
+  },
+
+//UI
+load: function(){
+this.data = data.songs;
+data.songs.forEach(function(val, i){
+  $("")
+})
+}
+};
---
+++ assets/JS/playlist.js
@@ -0,0 +1,9 @@
+var data = {
+  "songs" : [
+    {
+      "fileName" : "http://picosong.com/6rv6/",
+      "artist" : "Ty Segall",
+      "song"  : "Wave Goodbye"
+    }
+  ]
+};
