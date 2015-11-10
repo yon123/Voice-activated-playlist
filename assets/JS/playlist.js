af9d13997bcfd03134bf19e62071c5928247d3e1
Yon <yaguilar213@gmail.com>
2015-11-10 15:51:30 -0800

    added drake

--- assets/JS/playlist.js
+++ assets/JS/playlist.js
@@ -1,9 +1,9 @@
 var data = {
   "songs" : [
     {
-      "fileName" : "http://www.filedropper.com/01timecollapseiithe7thterror",
-      "artist" : "Fuzz",
-      "songName"  : "7th Terror"
+      "fileName" : "http://onlinekaraoke.tv/cache/files2/hotline-bling-drake-hotline-bling-drake-hotline-bling-drake.mp3",
+      "artist" : "Drake",
+      "songName"  : "HOTLINE BLING"
     }
   ]
 };
