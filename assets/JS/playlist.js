e30f572c3e143729f4c71799d3adfade3f8f8ad0
Yon <yaguilar213@gmail.com>
2015-11-11 10:05:13 -0800

    Added more css

--- assets/JS/playlist.js
+++ assets/JS/playlist.js
@@ -22,7 +22,64 @@ var data = {
     "fileName" : "http://mp3act.com/storage/mp3/f7c87fa8d3ab99cba0c44b030322bbe5/tame-impala-the-less-i-know-the-better.mp3",
     "artist" : "Tame Impala",
     "songName" : "The Less I Know The Better"
-  }
+  },
+
+  {
+    "fileName" : "http://gimmetinnitus.com/blog/wp-content/uploads/2012/04/06%20Wave%20Goodbye.mp3",
+    "artist" : "Ty Segall Band",
+    "songName" : "Wave Goodbye"
+
+  },
+
+  {
+
+    "fileName": "http://bankrobbermusic.com/issue/2011/6/05%20I%20Can/'t%20Feel%20It.mp3",
+    "artist" : "Ty Segall",
+    "songName" : "I Can't Feel It"
+  },
+
+  {
+    "fileName": "http://mp3light.net/assets/songs/95000-95999/95353-space-oddity-david-bowie--1411591987.mp3",
+    "artist" : "David Bowie",
+    "songName" : "Space Oddity"
+  },
+
+  {
+    "fileName" : "http://mp3light.net/assets/songs/68000-68999/68067-modern-love-david-bowie--1411584773.mp3",
+    "artist" : "David Bowie",
+    "songName" : "Modern Love"
+  },
+
+  {
+    "fileName" : "http://liquidgeneration.blogs.com/music/files/Starman.mp3",
+    "artist" : "David Bowie",
+    "songName" : "Starman"
+  },
+
+{
+  "fileName" : "http://fridaymixtape.net/mixtape/songs/186/To%20The%20Boy%20I%20Jumped%20In%20The%20Hemlock%20Alley.mp3",
+  "artist" : "White Fence",
+  "songName" : "The Boy I Jumped In The Hemlock Alley"
+},
+
+{
+  "fileName" : "http://blogfiles.wfmu.org/GG/Wanda_Jackson_-_Funnel_Of_Love_45_played_at_33rpm.mp3",
+  "artist" : "Wanda Jackson",
+  "songName" : "Funnel of Love(played at 33rpm)"
+},
+
+{
+    "fileName" : "http://www.frockstarvintage.com/The_Velvet_Underground_-_Rock_And_Roll.mp3",
+    "artist" : "The Velvet Underground",
+    "songName" : "Rock 'n Roll"
+},
+
+{
+
+  "fileName" : "http://hanguyen82.free.fr/anh/Nhac/Pale%20Blue%20Eyes.mp3",
+  "artist" : "The Velvet Underground",
+  "songName" : "Pale Blue Eyes"
+}
 
   ]
 };
