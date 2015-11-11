574a676646c10f339502c258884b4f919a73aadf
Yon <yaguilar213@gmail.com>
2015-11-10 16:01:25 -0800

    added new track

--- assets/JS/playlist.js
+++ assets/JS/playlist.js
@@ -4,6 +4,12 @@ var data = {
       "fileName" : "http://onlinekaraoke.tv/cache/files2/hotline-bling-drake-hotline-bling-drake-hotline-bling-drake.mp3",
       "artist" : "Drake",
       "songName"  : "HOTLINE BLING"
+    },
+
+    {
+      "fileName" : "http://a.tumblr.com/tumblr_m0o5h6YPB21qdzicwo1.mp3",
+      "artist" : "Dr. Dre",
+      "songName" : "Xplosive"
     }
   ]
 };
