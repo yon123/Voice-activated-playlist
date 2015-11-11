c64d324b80ad3e3753569f8d90ec7b2f5fa72af4
Yon <yaguilar213@gmail.com>
2015-11-10 17:18:10 -0800

    added more styling

--- assets/JS/playlist.js
+++ assets/JS/playlist.js
@@ -10,6 +10,19 @@ var data = {
       "fileName" : "http://a.tumblr.com/tumblr_m0o5h6YPB21qdzicwo1.mp3",
       "artist" : "Dr. Dre",
       "songName" : "Xplosive"
-    }
+    },
+
+    {
+      "fileName" : "http://themusicslut.com/wp-content/uploads/2008/11/tame_impala_remember_m.mp3",
+      "artist" : "Tame Impala",
+      "songName" : "Remember Me"
+    },
+
+    {
+    "fileName" : "http://mp3act.com/storage/mp3/f7c87fa8d3ab99cba0c44b030322bbe5/tame-impala-the-less-i-know-the-better.mp3",
+    "artist" : "Tame Impala",
+    "songName" : "The Less I Know The Better"
+  }
+
   ]
 };
